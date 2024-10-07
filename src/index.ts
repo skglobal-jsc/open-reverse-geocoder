817c1cc478a5108d21849184423a0db69e7d339c
Binh Bui <bvbinh@sk-global.biz>
2024-10-07 11:41:05 +0700

    first release

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,11 @@
 import { lngLatToGoogle } from 'global-mercator';
 import axios from 'axios';
 import { setupCache } from 'axios-cache-interceptor';
-import { ReverseGeocodingOptions, ReverseGeocodingResult } from './interfaces';
+import {
+  ReverseGeocodingOptions,
+  ReverseGeocodingResult,
+  ReverseGeocodingResultJP,
+} from './interfaces';
 import countryOptions from './countryOptions';
 import { getTile, getTileResult } from './utils';
 
@@ -26,6 +30,13 @@ api.interceptors.request.use((config) => {
 
 const defaultOptions = countryOptions.DEFAULT;
 
+/**
+ * Performs reverse geocoding to obtain location information based on longitude and latitude.
+ *
+ * @param lnglat - The longitude and latitude coordinates.
+ * @param options - Optional parameters for reverse geocoding.
+ * @returns A promise that resolves to the reverse geocoding result.
+ */
 const openReverseGeocoder = async (
   lnglat: LngLat,
   options?: ReverseGeocodingOptions
@@ -36,4 +47,96 @@ const openReverseGeocoder = async (
   return getTileResult(tile, x, y, lnglat, opt);
 };
 
-export { openReverseGeocoder };
+/**
+ * Performs reverse geocoding using the GSI (Geospatial Information Authority of Japan) API.
+ * Given latitude and longitude coordinates, it returns the corresponding municipality code and city name.
+ *
+ * @param {Object} params - The parameters for the reverse geocoding request.
+ * @param {number} params.lat - The latitude coordinate.
+ * @param {number} params.lon - The longitude coordinate.
+ * @returns {Promise<ReverseGeocodingResultJP>} A promise that resolves to an object containing the municipality code and city name.
+ *
+ * @example
+ * const result = await gsiReverseGeocoder({ lat: 35.6895, lon: 139.6917 });
+ * console.log(result); // { code: '13101', city: 'Chiyoda-ku' }
+ */
+const gsiReverseGeocoder = async ({
+  lat,
+  lon,
+}: {
+  lat: number;
+  lon: number;
+}) => {
+  const response = await api.get(
+    'https://mreversegeocoder.gsi.go.jp/reverse-geocoder/LonLatToAddress',
+    {
+      responseType: 'json',
+      params: {
+        lat,
+        lon,
+      },
+    }
+  );
+  const result = response.data;
+  const {
+    results: { muniCd: code, lv01Nm: city },
+  } = result;
+  return { code, city } as ReverseGeocodingResultJP;
+};
+
+/**
+ * Retrieves the elevation for a given longitude and latitude using the GSI API.
+ *
+ * @param {Object} coordinates - The coordinates for which to get the elevation.
+ * @param {number} coordinates.lon - The longitude of the location.
+ * @param {number} coordinates.lat - The latitude of the location.
+ * @returns {Promise<{ longitude: number, latitude: number, elevation: number }>}
+ * A promise that resolves to an object containing the longitude, latitude, and elevation.
+ */
+const getElevation = async ({ lon, lat }: { lon: number; lat: number }) => {
+  // first, get elevation from GSI
+  const response = await api.get(
+    'https://mreversegeocoder.gsi.go.jp/general/dem/scripts/getelevation.php',
+    {
+      responseType: 'json',
+      params: {
+        lat,
+        lon,
+      },
+    }
+  );
+
+  const elevation = parseFloat(response.data.elevation);
+  return { longitude: lon, latitude: lat, elevation };
+};
+
+/**
+ * Reverse geocodes the given longitude and latitude to obtain a city code.
+ *
+ * @param {Object} params - The parameters for reverse geocoding.
+ * @param {number} params.lon - The longitude of the location.
+ * @param {number} params.lat - The latitude of the location.
+ *
+ * @returns {Promise<ReverseGeocodingResult>} A promise that resolves to the reverse geocoding result.
+ *
+ * @throws Will attempt to use an alternative geocoding service if the primary service fails.
+ */
+const reverseGeocoder = async ({
+  lon,
+  lat,
+}: {
+  lon: number;
+  lat: number;
+}): Promise<ReverseGeocodingResult> => {
+  // first, get city code from open reverse geocoder(local)
+  try {
+    const result = await openReverseGeocoder([lon, lat]);
+    return result;
+  } catch (error) {
+    // if failed, get city code from GSI
+    console.log('Failed to get city code from open reverse geocoder');
+    return gsiReverseGeocoder({ lat, lon });
+  }
+};
+
+export { reverseGeocoder, getElevation };
--- src/local.mjs
+++ src/local.mjs
@@ -1,11 +1,41 @@
-import { openReverseGeocoder } from "../dist/index.modern.mjs";
+import {
+  latLonToAddress,
+  getElevation,
+  gsiReverseGeocoder,
+} from '../dist/index.modern.mjs';
 
 const run = async () => {
   console.log('Test open reverse geocoder');
-  console.time('openReverseGeocoder');
-  const result = await openReverseGeocoder([137.725940, 34.710896])
+  const lat = 34.710896;
+  const lon = 137.72594;
+
+  console.log('Test get elevation');
+  console.time('getElevation');
+  const elevation = await getElevation({
+    lat,
+    lon,
+  });
+  console.log(elevation);
+  console.timeEnd('getElevation');
+
+  console.time('latLonToAddress');
+
+  const result = await latLonToAddress({
+    lat,
+    lon,
+  });
   console.log(result);
-  console.timeEnd('openReverseGeocoder');
+  console.timeEnd('latLonToAddress');
+
+  // gsiReverseGeocoder
+  console.log('Test gsi reverse geocoder');
+  console.time('gsiReverseGeocoder');
+  const gsiResult = await gsiReverseGeocoder({
+    lat,
+    lon,
+  });
+  console.log(gsiResult);
+  console.timeEnd('gsiReverseGeocoder');
 };
 
 run();
